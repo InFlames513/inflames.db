7a331da7750470babd4d6c9921f5ce76d2da9f99
InFlames <85372327+InFlames513@users.noreply.github.com>
2023-06-27 17:25:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const tool = require("rexar-tools").default;
+const { stripIndent } = require("common-tags");
+const colors = require('colors');
+const thisVersion = require("./package.json").version;
+tool.npm("inflames.db").then(m => {
+    const latestVersion = m.version;
+    if(thisVersion !== latestVersion) {
+        console.log(stripIndent`inflames.db New Version Available!`.red.bold);
+        console.log(stripIndent`${thisVersion} => ${latestVersion}`.blue.bold)
+    }
+});
+
 /**
  * Allows you to use bson as database file.
  */
-exports.bson = () => require("./src/drivers/bson");
+exports.bson = (file="inflames") => require("./src/drivers/bson")(file);
 
 /**
  * Allows you to use sqlite as database file.
  */
-exports.sqlite = () => require("./src/drivers/sqlite");
+exports.sqlite = (file="inflames") => require("./src/drivers/sqlite")(file);
